024f638839b2913f1c25d309608921a9a3e09914
vishwas1143 <137210183+vishwas1143@users.noreply.github.com>
2025-03-01 11:36:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function insert_Row() {
-    //Write your code here
-  
-  
+var table = document.getElementById("sampleTable");
+let row=table.insertRow(0);
+let cell1=row.insertCell(0);
+let cell2 =row.insertCell(1);
+cell1.innerHTML='New Cell1';
+cell2.innerHTML='New Cell2';
 }
